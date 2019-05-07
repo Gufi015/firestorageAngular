89eb04748ecd85d105a1e87c915346a7efced618
Rufino Rojas <lucras015@hotmail.com>
2019-05-07 16:08:04 -0500

    Subir fotos a firebase storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import { FirebaseStorageService } from "./firebase-storage.service";
+import { Component } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'CloudStorage';
+  title = "CloudStorage";
+
+  public archivoForm = new FormGroup({
+    archivo: new FormControl(null, Validators.required)
+  });
+
+  public mensajeArchivo = "No hay un archivo seleccionado";
+  public datosFormulario = new FormData();
+  public nombreArchivo = '';
+  public URLPublica = '';
+  public porcentaje = 0;
+  public finalizado = false;
+
+  constructor(private firebaseStorage: FirebaseStorageService) {}
+
+  public cambioArchivo(event) {
+    if (event.target.files.length > 0) {
+      for (let i = 0; i < event.target.files.length; i++) {
+        this.mensajeArchivo = `Archivo Preparado: ${
+          event.target.files[i].name
+        }`;
+        this.nombreArchivo = event.target.files[i].name;
+        this.datosFormulario.delete('archivo');
+        this.datosFormulario.append(
+          'archivo',
+          event.target.files[i],
+          event.target.files[i].name
+        );
+      }
+    } else {
+      this.mensajeArchivo = "No hay un Archivo selecionado!";
+    }
+  }
+  public subirArchivo() {
+    let archivo = this.datosFormulario.get('archivo');
+    let referencia = this.firebaseStorage.referenciaCloudStorage(
+      this.nombreArchivo
+    );
+    let tarea = this.firebaseStorage.tareaCloudStorage(
+      this.nombreArchivo,
+      archivo
+    );
+
+    tarea.percentageChanges().subscribe(porcentaje => {
+      this.porcentaje = Math.round(porcentaje);
+      if (this.porcentaje == 100) {
+        this.finalizado = true;
+        console.log('Subida Finalizada');
+      }
+    }, (error)=>{
+      console.log(error);
+    });
+
+
+    referencia.getDownloadURL().subscribe((URL) => {
+      this.URLPublica = URL;
+      console.log('Esta es la url' + JSON.stringify(URL));
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+import { AngularFireModule} from '@angular/fire';
+import { AngularFireStorageModule} from '@angular/fire/storage';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -10,7 +14,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireStorageModule,
+    ReactiveFormsModule,
+    FormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
